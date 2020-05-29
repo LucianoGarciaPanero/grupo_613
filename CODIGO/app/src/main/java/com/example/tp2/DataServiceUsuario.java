6c0c5b36b217cabdb9c0a9c156fbfc672d7b830d
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-05-29 19:41:37 -0300

    Hice la lógica para registrar usuario en ambiente de test

--- CODIGO/app/src/main/java/com/example/tp2/DataServiceUsuario.java
+++ CODIGO/app/src/main/java/com/example/tp2/DataServiceUsuario.java
@@ -1,8 +1,16 @@
 package com.example.tp2;
 
+import android.text.InputType;
+
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class DataServiceUsuario {
@@ -18,14 +26,44 @@ public class DataServiceUsuario {
             throw new PassException("La contraseña debe contener al menos 8 caracteres");
         }
 
-        
+        // A partir de los datos del formulario busco crear un objeto json.
+        FormularioUsuario fs = new FormularioUsuario(env, name, lastname, dni, email, password, comission, group);
+        Gson json = new Gson();
+        String usuarioString = json.toJson(fs);
 
-        URL url = new URL("http://so-unlam.net.ar/api/api/register");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("POST");
-
-
-
+        // Establezco la conexión y le mando el json.
+        URL url = null;
+        HttpURLConnection con = null;
+        try {
+            url = new URL("http://so-unlam.net.ar/api/api/register");
+            con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type","application/json");
+            con.connect();
+            OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
+            out.write(usuarioString);
+            out.close();
+            int res = con.getResponseCode();
+            if(res == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader( new InputStreamReader(con.getInputStream(), "utf-8"));
+                String line = null;
+                while ((line = br.readLine()) != null){
+                    System.out.println(line);
+                }
+                br.close();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(con != null) {
+                con.disconnect();
+            }
+        }
 
         return new Usuario(name,lastname,dni,email,comission,group);
     }
