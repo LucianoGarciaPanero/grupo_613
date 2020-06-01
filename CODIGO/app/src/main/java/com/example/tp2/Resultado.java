09f88ead3963c5a03f8174dae8cfdb241ce1f8cb
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-05-31 21:31:10 -0300

    Definicion de resultado

--- CODIGO/app/src/main/java/com/example/tp2/Resultado.java
+++ CODIGO/app/src/main/java/com/example/tp2/Resultado.java
@@ -1,5 +1,12 @@
 package com.example.tp2;
 
-public class Resultado {
+/*
+Existe un record unicamente por los puntos, pero habra uno por cada categoría
+*/
 
+public class Resultado {
+    private int puntos;
+    private float tiempo; //Tiempo en que rompes la lata
+    private float acleracionMax;
+    private String dureza;
 }
