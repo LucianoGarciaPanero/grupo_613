d6bdfb293aadd25bcec900d1d4a6383a2b7adaf5
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-06-01 10:59:31 -0300

    Agregue enum para la dificultad

--- CODIGO/app/src/main/java/com/example/tp2/Partida/ConfigurarPartidaActivity.java
+++ CODIGO/app/src/main/java/com/example/tp2/Partida/ConfigurarPartidaActivity.java
@@ -3,14 +3,28 @@ package com.example.tp2.Partida;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.tp2.R;
 
 public class ConfigurarPartidaActivity extends AppCompatActivity {
 
+    // Objetos de la GUI
+    private Spinner comboDificultad;
+    private Spinner comboFondo;
+    private Button buttonIniciarPartida;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurar_partida);
+
+        this.buttonIniciarPartida = findViewById(R.id.buttonIniciarPartida);
+        this.comboDificultad = findViewById(R.id.comboDificultad);
+        this.comboFondo = findViewById(R.id.comboFondo);
+
+
     }
 }
