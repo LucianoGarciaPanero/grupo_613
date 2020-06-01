e4fced4f83656d8cc095c3650992d4479932ac41
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-06-01 08:59:00 -0300

    Agregue clase para realizar serializacion con gson

---
+++ CODIGO/app/src/main/java/com/example/tp2/Evento.java
@@ -0,0 +1,48 @@
+package com.example.tp2;
+
+public class Evento {
+
+    private String env;
+    private String type_events;
+    private String state;
+    private String description;
+
+    public Evento(String env, String type_events, String state, String description) {
+        this.env = env;
+        this.type_events = type_events;
+        this.state = state;
+        this.description = description;
+    }
+
+    public String getEnv() {
+        return env;
+    }
+
+    public void setEnv(String env) {
+        this.env = env;
+    }
+
+    public String getType_events() {
+        return type_events;
+    }
+
+    public void setType_events(String type_events) {
+        this.type_events = type_events;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
