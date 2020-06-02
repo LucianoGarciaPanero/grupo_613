7c4a35afae0fba22b8ac8d62df2e2ca9cef83a1c
TomasSG <tomassanchezgrigioni98@gmail.com>
2020-06-02 11:42:34 -0300

    Hice el diseño de la activity de historial de resultados

--- CODIGO/app/src/main/java/com/example/tp2/Menu/ResultadoActivity.java
+++ CODIGO/app/src/main/java/com/example/tp2/Menu/ResultadoActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.tp2.R;
@@ -14,6 +15,13 @@ public class ResultadoActivity extends AppCompatActivity {
 
     // Objetos de la GUI
     private TextView labelTipoResultado;
+    private TextView labelSeleccioneDificultad;
+    private TextView puntos;
+    private TextView tiempo;
+    private TextView aceleracionMaxima;
+    private TextView dificultad;
+    private TextView labelError;
+    private Spinner comboDificultad;
     private Button buttonVolver;
 
     private String accion;
@@ -25,6 +33,13 @@ public class ResultadoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_resultado);
 
         this.labelTipoResultado = findViewById(R.id.labelTipoResultado);
+        this.labelSeleccioneDificultad = findViewById(R.id.labelSeleccioneDificultad);
+        this.puntos = findViewById(R.id.puntos);
+        this.tiempo = findViewById(R.id.tiempo);
+        this.aceleracionMaxima = findViewById(R.id.aceleracionMax);
+        this.dificultad = findViewById(R.id.dificultad);
+        this.labelError = findViewById(R.id.labelError);
+        this.comboDificultad = findViewById(R.id.comboDificultad);
         this.buttonVolver = findViewById(R.id.buttonVolver);
 
         this.buttonVolver.setOnClickListener(new View.OnClickListener() {
